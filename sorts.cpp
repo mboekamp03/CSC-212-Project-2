20ee4b88f8c89099ab395db1effbc53760918104
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-07-08 10:08:16 -0400

    Update sorts.cpp

--- sorts.cpp
+++ sorts.cpp
@@ -16,4 +16,33 @@ void Sorts::insertionSort(vector <int> &insertionSequence, int sizeN){
     }
 }
 
+void Sorts::mergeSort(std::vector<int>& arr, int left, int mid, int right) {
+    int i = left;       // Index for left subarray
+    int j = mid + 1;    // Index for right subarray
+    int k = 0;          // Index for temporary array
+
+    std::vector<int> temp(right - left + 1); // Temporary array
+
+    // Merge the two subarrays into the temporary array
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    // Copy the remaining elements of the left subarray, if any
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    // Copy the remaining elements of the right subarray, if any
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    // Copy the merged elements back to the original array
+    for (i = left, k = 0; i <= right; i++, k++)
+        arr[i] = temp[k];
+}
+
+
 
