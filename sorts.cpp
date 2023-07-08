87e3b3896fcb94944e5a2f6c1eb3e57de955c260
schadha009 <66641207+schadha009@users.noreply.github.com>
2023-07-08 10:06:55 -0400

    Update sorts.cpp

--- sorts.cpp
+++ sorts.cpp
@@ -1,1 +1,19 @@
-#include <iostream>
+#include "gradebook.h"
+#include <fstream>
+
+//insertion sort function, where insertionSequence is the vector of size sizeN
+void Sorts::insertionSort(vector <int> &insertionSequence, int sizeN){
+
+    int i, j, k = 0;
+    for (i = 1; i < sizeN; i++){
+        j = i;
+        k = insertionSequence[i];
+        while (j > 0 and k < insertionSequence[j - 1]){
+            insertionSequence[j] = insertionSequnece[j - 1];
+            j--;
+        }
+        insertionSequence[j] = k;
+    }
+}
+
+
