f0ec57b2711c6e8156b4cf40c8d48a3be3f881d8
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-07-19 13:42:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
-#include <fstream>
 #include <vector>
-
-
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include "sort.h"
 
 int main(int argc, char*argv[]) {
+    Sorts sort;
 
+    std::string fileName = argv[1];
+    std::ifstream inputFile(fileName); 
+    std::string line;
+    std::getline(inputFile, line);
+    std::istringstream data(line);
+
+    int mode = std::stoi(argv[2]);
+    sort.setMode(mode);
+
+    int value;
+    while (data >> value) {
+        sort.insert(value);
+    }
+
+
+    sort.initializeVisual();
+
+    return 0;
 }
