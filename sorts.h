063dbbd7ed90db78a76e8a728ecd59f240a17947
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-07-22 16:28:51 -0400

    Update sorts.h

--- sorts.h
+++ sorts.h
@@ -4,10 +4,7 @@
 #include <random>
 #include <string>
 
-
-
 class Sorts {
-
 private:
 
     // Vectors 
@@ -16,7 +13,6 @@ private:
     std::vector<int> mergeSequence;
     std::vector<int> quickSequence;
     std::vector<int> radixSequence;
-
 
     // Variables
     int sizeN;
@@ -27,25 +23,24 @@ private:
     int SCREEN_WIDTH = 800;
     int SCREEN_HEIGHT = 600;
     int FRAME_DELAY = 100;
-    int GAP = 0;
+    int GAP = 2;
 
     int mode;
 
-
 public:
     //insertions sort methods
-    void insertionSort(std::vector<int> &insertionSequence, int n);
+    void insertionSort(std::vector<int> &insertionSequence, int n, SDL_Renderer *renderer);
 
     //mergesort methods
     void merge(std::vector<int> &arr, int left, int mid, int right);
     void mergeSort(std::vector<int> &arr, int left, int right, SDL_Renderer *renderer);
 
     //quicksort methods
-    int quickSort(std::vector <int> *quick, int left, int right);
-    void r_quicksort(std::vector<int> *quick, int left, int right);
+    int quickSort(std::vector <int> *quick, int left, int right, SDL_Renderer *renderer);
+    void r_quicksort(std::vector<int> *quick, int left, int right, SDL_Renderer *renderer);
 
     // radix sort methods
-    void radixSort(std::vector<int> &radixSequence, int n);
+    void radixSort(std::vector<int> &radixSequence, int n, SDL_Renderer *renderer);
 
     // Fill sequence vector
     void insert(int num);
