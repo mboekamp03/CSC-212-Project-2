66e4db0ebd8ad6af530f59030efa8e63640ea525
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-07-24 15:25:04 -0400

    Update sorts.h

--- sorts.h
+++ sorts.h
@@ -1,59 +1,31 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <SDL2/SDL.h>
 #include <random>
-#include <string>
+#include <cmath>
+
 
 class Sorts {
+
 private:
-
-    // Vectors 
-    std::vector<int> sequence;
-    std::vector<int> insertionSequence;
-    std::vector<int> mergeSequence;
-    std::vector<int> quickSequence;
-    std::vector<int> radixSequence;
-
-    // Variables
-    int sizeN;
-    int hi;
-    int mid;
-    int low;
-
-    int SCREEN_WIDTH = 800;
-    int SCREEN_HEIGHT = 600;
-    int FRAME_DELAY = 100;
-    int GAP = 2;
-
-    int mode;
+    // Helper Functions
+    static void mergeSortHelper(std::vector<int> &arr, int left, int mid, int right);
+    static int quickSortHelper(std::vector<int> &arr, int left, int right);
+    static void quickSort(std::vector<int> &arr, int left, int right);
 
 public:
-    //insertions sort methods
-    void insertionSort(std::vector<int> &insertionSequence, int n, SDL_Renderer *renderer);
+    // Insertion Sort
+    static void insertionSort(std::vector<int> &arr);
 
-    //mergesort methods
-    void merge(std::vector<int> &arr, int left, int mid, int right);
-    void mergeSort(std::vector<int> &arr, int left, int right, SDL_Renderer *renderer);
+    // Merge Sort
+    static void mergeSort(std::vector<int> &arr);
+    static void mergeSort(std::vector<int> &arr, int left, int right);
 
-    //quicksort methods
-    int quickSort(std::vector <int> *quick, int left, int right, SDL_Renderer *renderer);
-    void r_quicksort(std::vector<int> *quick, int left, int right, SDL_Renderer *renderer);
+    // QuickSort
+    static void quickSort(std::vector<int> &arr);
 
     // radix sort methods
-    void radixSort(std::vector<int> &radixSequence, int n, SDL_Renderer *renderer);
+    static void radixSort(std::vector<int> &arr);
+    static void radixSort(std::vector<int> &arr, int maxDigits);
 
-    // Fill sequence vector
-    void insert(int num);
-
-    // Intialize all Vectors
-    void intializeVectors(std::vector<int> sequence);
-
-    // Visualizer
-    bool initializeSDL(SDL_Window *&window, SDL_Renderer *&renderer);
-    void handleEvents(bool &quit);
-    void renderArray(SDL_Renderer *renderer, const std::vector<int> &arr);
-    void initializeVisual();
-
-    // Mode
-    void setMode(int givenMode);
 };
